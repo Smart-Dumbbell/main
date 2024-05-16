bc18fba8e5268e8739ad041282e16e46aff5b30f
Duo Yu <81913114+yd295826320@users.noreply.github.com>
2024-05-16 12:52:21 -0700

    brandon (#2)
    Co-authored-by: Brandon Lee <124757679+bjlee97@users.noreply.github.com>

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,28 +1,21 @@
-//import 'dart:html';
 import 'dart:math';
 import 'dart:ui';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
- 
 import 'package:flutter/cupertino.dart';
-//import 'package:smart_dumbbell_mobile/bar_graphs/bar_graph.dart';
 import 'package:provider/provider.dart';
- import 'package:smart_dumbbell_mobile/start_page.dart';
+import 'package:smart_dumbbell_mobile/start_page.dart';
 import 'package:smart_dumbbell_mobile/working_page.dart';
 import 'package:smart_dumbbell_mobile/me_page.dart';
 import 'package:smart_dumbbell_mobile/goal_page.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
 import 'package:logger/logger.dart' as MyLogger;
 import 'package:smart_dumbbell_mobile/progress_page.dart';
 
 
 ValueNotifier<bool> isBluetoothConnected = ValueNotifier(false);
-
-var _value = '';
-
 
 void main() {
   runApp(
@@ -31,7 +24,6 @@ void main() {
       child: MyApp(),
     ),
   );
-  //runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -45,160 +37,13 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
-        // '/start': (context) => StartPage(),
         '/me': (context) => MePage(),
         '/progress': (context) => ProgressPage(),
-        //'/goal': (context) => GoalPage(onGoalSelected: (int ) {  },),
         '/goal': (context) => GoalPage(onGoalSelected: (int selectedIndex) {}),
-
       },
     );
   }
 }
-
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   int _selectedIndex = 0;
-//   final _ble = FlutterReactiveBle();
-//   StreamSubscription<DiscoveredDevice>? _scanSub;
-//   StreamSubscription<ConnectionStateUpdate>? _connectSub;
-//   StreamSubscription<List<int>>? _notifySub;
-//   var _found = false;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
-//   }
-
-//   @override
-//   void dispose() {
-//     _notifySub?.cancel();
-//     _connectSub?.cancel();
-//     _scanSub?.cancel();
-//     super.dispose();
-//   }
-
-//   void _onItemTapped(int index) {
-//     if (index >= 0 && index < 4) {
-//       setState(() {
-//         _selectedIndex = index;
-//       });
-//     }
-//   }
-
-//   void _onScanUpdate(DiscoveredDevice d) {
-//     if (d.name == 'BLE-TEMP' && !_found) {
-//       _found = true;
-//       _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
-//         if (update.connectionState == DeviceConnectionState.connected) {
-//           setState(() {
-//             isBluetoothConnected.value  = true;
-//           });
-//           _onConnected(d.id);
-//         }
-//         else {
-//           isBluetoothConnected.value = false;
-//         }
-//       });
-//     }
-//   }
-
-//   void _onConnected(String deviceId) {
-//     final characteristic = QualifiedCharacteristic(
-//         deviceId: deviceId,
-//         serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
-//         characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'));
-
-//     _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
-//       setState(() {
-//         _value = const Utf8Decoder().convert(bytes);
-//       });
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         //title: Text('Home Page'),
-//         automaticallyImplyLeading: false, // Remove the back arrow
-//       ),
-//       body: Center(
-//         child: _selectedIndex == 0
-//             ? ElevatedButton(
-//                 onPressed: () {
-//                   // Your code here for Start button
-//                   Navigator.push(
-//                     context,
-//                     MaterialPageRoute(builder: (context) => WorkingPage()),
-//                   );
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   shape: CircleBorder(),
-//                   padding: EdgeInsets.all(120), // Adjust the size of the button
-//                 ),
-//                 child: Text('START', style: TextStyle(fontSize: 50)), // Adjust the size of the font
-//               )
-//             : _value.isEmpty
-//                 ? const CircularProgressIndicator()
-//                 : Text(_value, style: Theme.of(context).textTheme.titleLarge),
-//       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         type: BottomNavigationBarType.fixed,
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.account_circle),
-//             label: 'Me',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.report),
-//             label: 'Progress',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.fitness_center),
-//             label: 'Goal',
-//           ),
-//         ],
-//         currentIndex: _selectedIndex,
-//         selectedItemColor: Colors.black,
-//         onTap: _onItemTapped,
-//       ),
-//       floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-//       floatingActionButton: Padding(
-//         padding: const EdgeInsets.only(top: 20, right: 20),
-//         child: ValueListenableBuilder<bool>(
-//           valueListenable: isBluetoothConnected,
-//           builder: (context, value, child) {
-//             return IconButton(
-//               icon: Icon(
-//                 Icons.bluetooth,
-//                 color: value ? Colors.blue : Colors.red,
-//                 size: 30,
-//               ),
-//               onPressed: () {
-//                 _scanSub?.cancel();
-//                 _found = false;
-//                 setState(() {});
-//                 _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
-//               },
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 
 class HomePage extends StatefulWidget {
   @override
@@ -207,31 +52,76 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  final _ble = FlutterReactiveBle();
+  StreamSubscription<DiscoveredDevice>? _scanSub;
+  StreamSubscription<ConnectionStateUpdate>? _connectSub;
+  StreamSubscription<List<int>>? _notifySub;
+  var _found = false;
 
   static List<Widget> _widgetOptions = <Widget>[
     StartPage(),
     MePage(),
     ProgressPage(),
-    //GoalPage(),
     GoalPage(onGoalSelected: (int selectedIndex) {}),
   ];
 
-  void _onItemTapped(int index) {
-
-    if (index >= 0 && index < _widgetOptions.length) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
   }
+
+  @override
+  void dispose() {
+    _notifySub?.cancel();
+    _connectSub?.cancel();
+    _scanSub?.cancel();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    if (index >= 0 && index < _widgetOptions.length) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  void _onScanUpdate(DiscoveredDevice d) {
+    if (d.name == 'BLE-TEMP' && !_found) {
+      _found = true;
+      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+        if (update.connectionState == DeviceConnectionState.connected) {
+          setState(() {
+            isBluetoothConnected.value = true;
+          });
+          _onConnected(d.id);
+        } else {
+          isBluetoothConnected.value = false;
+        }
+      });
+    }
+  }
+
+  void _onConnected(String deviceId) {
+    final characteristic = QualifiedCharacteristic(
+      deviceId: deviceId,
+      serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+      characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
+    );
+
+    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+      setState(() {
+        // handle bytes if needed
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
-        //title: Text('Home Page'),
-        automaticallyImplyLeading: false, // Remove the back arrow
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -263,4 +153,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
--- Mobile/lib/start_page.dart
+++ Mobile/lib/start_page.dart
@@ -1,12 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:smart_dumbbell_mobile/main.dart';
+import 'package:smart_dumbbell_mobile/main.dart'; // Import main.dart for isBluetoothConnected
 import 'package:smart_dumbbell_mobile/working_page.dart';
-import 'dart:convert';
 import 'dart:async';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'package:logger/logger.dart' as MyLogger;
+import 'package:logger/logger.dart' as myLogger;
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final _ble = FlutterReactiveBle();
+
+  StreamSubscription<DiscoveredDevice>? _scanSub;
+  StreamSubscription<ConnectionStateUpdate>? _connectSub;
+  StreamSubscription<List<int>>? _notifySub;
+
+  var _found = false;
+  var _isLoading = false;
+  final logger = myLogger.Logger();
+
+  @override
+  void dispose() {
+    _notifySub?.cancel();
+    _connectSub?.cancel();
+    _scanSub?.cancel();
+    super.dispose();
+  }
+
+  void _onScanUpdate(DiscoveredDevice d) {
+    if (d.name == 'BLE-TEMP' && !_found) {
+      logger.d('Scan done!');
+      _found = true;
+      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+        if (update.connectionState == DeviceConnectionState.connected) {
+          setState(() {
+            isBluetoothConnected.value = true;
+            _isLoading = false;
+          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => WorkingPage()),
+          );
+          _onConnected(d.id);
+        } else if (update.connectionState == DeviceConnectionState.disconnected) {
+          setState(() {
+            isBluetoothConnected.value = false;
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
+
+  void _onConnected(String deviceId) {
+    logger.d('On Connect!');
+    final characteristic = QualifiedCharacteristic(
+      deviceId: deviceId,
+      serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+      characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
+    );
+
+    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+      setState(() {
+        // handle bytes if needed
+      });
+    });
+  }
+
+  void _startBluetoothScan() {
+    _scanSub?.cancel();
+    _found = false;
+    setState(() {
+      _isLoading = true;
+    });
+    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +91,7 @@ class StartPage extends StatelessWidget {
             alignment: Alignment.center,
             child: ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => WorkingPage()),
-                );
+                _startBluetoothScan();
               },
               style: ElevatedButton.styleFrom(
                 shape: CircleBorder(),
@@ -32,151 +100,16 @@ class StartPage extends StatelessWidget {
               child: Text('START', style: TextStyle(fontSize: 50)), // Adjust the size of the font
             ),
           ),
-          // Bluetooth button
-          Positioned(
-            top: 20,
-            right: 20,
-            child: ValueListenableBuilder<bool>(
-              valueListenable: isBluetoothConnected,
-              builder: (context, value, child) {
-                return IconButton(
-                  icon: Icon(
-                    Icons.bluetooth,
-                    color: value ? Colors.blue : Colors.red,
-                    size: 30,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BluetoothPage(title: 'Demo')),
-                    );
-                  },
-                );
-              },
+          // Loading indicator
+          if (_isLoading)
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
             ),
-          ),
         ],
       ),
-    );
-  }
-}
-
-
-
-// class BluetoothPage extends StatefulWidget {
-//   @override
-//   _BluetoothPageState createState() => _BluetoothPageState();
-// }
-
-// class _BluetoothPageState extends State<BluetoothPage> {
-//   //bool isBluetoothConnected = false; // Step 4: Bluetooth connection state
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Bluetooth Page'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             ElevatedButton(
-//               onPressed: () {
-//                 isBluetoothConnected.value = true; // Update the ValueNotifier
-//                 Navigator.pop(context); // Go back to the home page
-//               },
-//               child: Text('Connect'),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 isBluetoothConnected.value = false; // Update the ValueNotifier
-//                 Navigator.pop(context); // Go back to the home page
-//               },
-//               child: Text('Disconnect'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-class BluetoothPage extends StatefulWidget {
-  const BluetoothPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<BluetoothPage> createState() => _BluetoothPageState();
-}
-
-class _BluetoothPageState extends State<BluetoothPage> {
-  final _ble = FlutterReactiveBle();
-
-  StreamSubscription<DiscoveredDevice>? _scanSub;
-  StreamSubscription<ConnectionStateUpdate>? _connectSub;
-  StreamSubscription<List<int>>? _notifySub;
-
-  var _found = false;
-  var _value = '';
-  
-
-  @override
-  initState() {
-    super.initState();
-    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
-  }
-
-  @override
-  void dispose() {
-    _notifySub?.cancel();
-    _connectSub?.cancel();
-    _scanSub?.cancel();
-    super.dispose();
-  }
-
-  final logger = MyLogger.Logger();
-
-  void _onScanUpdate(DiscoveredDevice d) {
-    // logger.d('On Scan!');
-    if (d.name == 'BLE-TEMP' && !_found) {
-      logger.d('Scan done!');
-      _found = true;
-      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
-        if (update.connectionState == DeviceConnectionState.connected) {
-          _onConnected(d.id);
-        }
-      });
-    }
-  }
-
-  void _onConnected(String deviceId) {
-    logger.d('On Connect!');
-    final characteristic = QualifiedCharacteristic(
-        deviceId: deviceId,
-        serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
-        characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'));
-
-    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
-      setState(() {
-        _value = const Utf8Decoder().convert(bytes);
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: _value.isEmpty
-              ? const CircularProgressIndicator()
-              : Text(_value, style: Theme.of(context).textTheme.titleLarge)),
     );
   }
 }
