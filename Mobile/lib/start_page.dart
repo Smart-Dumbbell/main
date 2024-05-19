ea491bdd58c8513502998e2851d999d7a635eef0
ANYKBZ <alanz@V.attlocal.net>
2024-05-18 23:44:19 -0700

    progress bar

--- Mobile/lib/start_page.dart
+++ Mobile/lib/start_page.dart
@@ -5,15 +5,15 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:logger/logger.dart' as myLogger;
-
-int repetitions = 0;
+import 'package:smart_dumbbell_mobile/global.dart';
+//int repetitions = 0;
 
 void updateRepetitions(int newReps) {
-  repetitions = newReps;
+  repetitions_count = newReps;
 }
 
 void resetRepetitions() {
-  repetitions = 0;
+  repetitions_count = 0;
 }
 
 class StartPage extends StatefulWidget {
@@ -84,8 +84,8 @@ class _StartPageState extends State<StartPage> {
     final regex = RegExp(r'\d+');
     final match = regex.firstMatch(data);
     if (match != null) {
-      final repetitions = int.parse(match.group(0)!);
-      updateRepetitions(repetitions);
+      final repetitions_count = int.parse(match.group(0)!);
+      updateRepetitions(repetitions_count);
     }
   }
 
@@ -134,3 +134,178 @@ class _StartPageState extends State<StartPage> {
     );
   }
 }
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:smart_dumbbell_mobile/main.dart';
+// import 'package:smart_dumbbell_mobile/working_page.dart';
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+// import 'package:logger/logger.dart' as myLogger;
+
+// int repetitions = 0;
+
+// void updateRepetitions(int newReps) {
+//   repetitions = newReps;
+// }
+
+// void resetRepetitions() {
+//   repetitions = 0;
+// }
+
+// class StartPage extends StatefulWidget {
+//   @override
+//   _StartPageState createState() => _StartPageState();
+// }
+
+// class _StartPageState extends State<StartPage> {
+//   final _ble = FlutterReactiveBle();
+//   StreamSubscription<DiscoveredDevice>? _scanSub;
+//   StreamSubscription<ConnectionStateUpdate>? _connectSub;
+//   StreamSubscription<List<int>>? _notifySub;
+//   var _found = false;
+//   var _isLoading = false;
+//   final logger = myLogger.Logger();
+//   int _progress = 0; // Progress variable
+
+//   @override
+//   void dispose() {
+//     _notifySub?.cancel();
+//     _connectSub?.cancel();
+//     _scanSub?.cancel();
+//     super.dispose();
+//   }
+
+//   void _onScanUpdate(DiscoveredDevice d) {
+//     if (d.name == 'BLE-TEMP' && !_found) {
+//       logger.d('Scan done!');
+//       _found = true;
+//       _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+//         if (update.connectionState == DeviceConnectionState.connected) {
+//           setState(() {
+//             isBluetoothConnected.value = true;
+//             _isLoading = false;
+//           });
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(builder: (context) => WorkingPage()),
+//           );
+//           _onConnected(d.id);
+//         } else if (update.connectionState == DeviceConnectionState.disconnected) {
+//           setState(() {
+//             isBluetoothConnected.value = false;
+//             _isLoading = false;
+//           });
+//         }
+//       });
+//     }
+//   }
+
+//   void _onConnected(String deviceId) {
+//     logger.d('On Connect!');
+//     final characteristic = QualifiedCharacteristic(
+//       deviceId: deviceId,
+//       serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+//       characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
+//     );
+
+//     _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+//       setState(() {
+//         value = const Utf8Decoder().convert(bytes);
+//         logger.d(value);
+//         _parseAndSaveRepetitions(value);
+//       });
+//     });
+//   }
+
+//   void _parseAndSaveRepetitions(String data) {    // parse string and save int
+//     final regex = RegExp(r'\d+');
+//     final match = regex.firstMatch(data);
+//     if (match != null) {
+//       final repetitions = int.parse(match.group(0)!);
+//       updateRepetitions(repetitions);
+//     }
+//   }
+
+//   void _startBluetoothScan() {
+//     _scanSub?.cancel();
+//     _found = false;
+//     setState(() {
+//       _isLoading = true;
+//     });
+//     _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
+//   }
+
+//   void _incrementProgress() {
+//     setState(() {
+//       if (_progress < 30) {
+//         _progress++;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Smart Dumbbell'),
+//       ),
+//       body: Stack(
+//         children: <Widget>[
+//           Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               // Clickable progress bar
+//               GestureDetector(
+//                 onTap: _incrementProgress,
+//                 child: Container(
+//                   width: 200, // Set the desired width
+//                   height: 10, // Set the desired height
+//                   child: LinearProgressIndicator(
+//                     value: _progress / 30,
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 10),
+//               // Display count below the progress bar
+//               Text(
+//                 'Count: $_progress',
+//                 style: TextStyle(fontSize: 24),
+//               ),
+//               // Start button
+//               Align(
+//                 alignment: Alignment.center,
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     resetRepetitions(); // Reset repetitions when starting a new workout
+//                     _startBluetoothScan();
+//                     _incrementProgress(); // Increment progress when button is pressed
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     shape: CircleBorder(),
+//                     padding: EdgeInsets.all(50), // Adjust the size of the button
+//                   ),
+//                   child: Text('START', style: TextStyle(fontSize: 30)), // Adjust the size of the font
+//                 ),
+//               ),
+//             ],
+//           ),
+//           // Loading indicator
+//           if (_isLoading)
+//             Container(
+//               color: Colors.white,
+//               child: Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
--- Mobile/lib/working_page.dart
+++ Mobile/lib/working_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_dumbbell_mobile/goal_page.dart';
 import 'package:smart_dumbbell_mobile/report_page.dart';
+import 'package:smart_dumbbell_mobile/global.dart';
 
 
 class WorkingPage extends StatefulWidget {
@@ -61,13 +62,49 @@ class _WorkingPageState extends State<WorkingPage> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  // Consumer<RepetitionsProvider>(
+                  //   builder: (context, repetitionsProvider, child) {
+                  //     return Text(
+                  //       'Goal Repetitions: ${repetitionsProvider.selectedRepetitions}',
+                  //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  //     );
+                  //   },
+                  // ),
+                  // GestureDetector(
+                  //   child: Container(
+                  //     width: 200, // Set the desired width
+                  //     height: 10, // Set the desired height
+                  //     child: LinearProgressIndicator(
+                  //       value: repetitions_count / repetitionsProvider.selectedRepetitions,
+                  //     ),
+                  //   ),
+                  // ),
                   Consumer<RepetitionsProvider>(
                     builder: (context, repetitionsProvider, child) {
-                      return Text(
-                        'Goal Repetitions: ${repetitionsProvider.selectedRepetitions}',
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      return Column(
+                        children: [
+                          Text(
+                            'Goal Repetitions: ${repetitionsProvider.selectedRepetitions}',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              width: 200, // Set the desired width
+                              height: 10, // Set the desired height
+                              child: LinearProgressIndicator(
+                                value: repetitions_count / repetitionsProvider.selectedRepetitions,
+                              ),
+                            ),
+                          ),
+                        ],
                       );
                     },
+                  ),
+                  SizedBox(height: 10),
+                  // Display count below the progress bar
+                  Text(
+                    'Count: $repetitions_count',
+                    style: TextStyle(fontSize: 24),
                   ),
                   CupertinoButton(
                     onPressed: () {
@@ -121,3 +158,147 @@ class _WorkingPageState extends State<WorkingPage> {
     ); 
   }
 }
+
+
+
+
+// import 'dart:async';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:smart_dumbbell_mobile/global.dart';
+// import 'package:smart_dumbbell_mobile/goal_page.dart';
+// import 'package:smart_dumbbell_mobile/report_page.dart';
+
+// class WorkingPage extends StatefulWidget {
+//   @override
+//   _WorkingPageState createState() => _WorkingPageState();
+// }
+
+// class _WorkingPageState extends State<WorkingPage> {
+//   late Stopwatch stopwatch;
+//   late Timer t;
+
+//   void handleStartStop() {
+//     if(stopwatch.isRunning) {
+//       stopwatch.stop();
+//     } else {
+//       stopwatch.start();
+//     }
+//   }
+
+//   String returnFormattedText() {
+//     var milli = stopwatch.elapsed.inMilliseconds;
+
+//     String milliseconds = (milli % 1000).toString().padLeft(3, "0");
+//     String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0");
+//     String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(2, "0");
+
+//     return "$minutes:$seconds:$milliseconds";
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     stopwatch = Stopwatch();
+
+//     t = Timer.periodic(Duration(milliseconds: 30), (timer) {
+//       setState(() {});
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     t.cancel();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Center(
+//           child: Stack(
+//             alignment: Alignment.center,
+//             children: [
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Consumer<RepetitionsProvider>(
+//                     builder: (context, repetitionsProvider, child) {
+//                       return Column(
+//                         children: [
+//                           Text(
+//                             'Goal Repetitions: ${repetitionsProvider.repetitions}',
+//                             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+//                           ),
+//                           SizedBox(height: 10),
+//                           LinearProgressIndicator(
+//                             value: repetitionsProvider.repetitions_count.toDouble() /
+//                                 repetitionsProvider.selectedRepetitions.toDouble(),
+//                             minHeight: 10,
+//                             backgroundColor: Colors.grey,
+//                             valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+//                           ),
+//                           SizedBox(height: 20),
+//                           Text(
+//                             'Current Repetitions: ${repetitionsProvider.repetitions}',
+//                             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+//                           ),
+//                         ],
+//                       );
+//                     },
+//                   ),
+//                   SizedBox(height: 20),
+//                   CupertinoButton(
+//                     onPressed: () {
+//                       handleStartStop();
+//                     },
+//                     padding: EdgeInsets.all(0),
+//                     child: Container(
+//                       height: 250,
+//                       alignment: Alignment.center,
+//                       decoration: BoxDecoration(
+//                         shape: BoxShape.circle,
+//                         border: Border.all(
+//                           color: Color(0xff0395eb),
+//                           width: 4,
+//                         ),
+//                       ),
+//                       child: Text(returnFormattedText(), style: TextStyle(
+//                         color: Colors.black,
+//                         fontSize: 40,
+//                         fontWeight: FontWeight.bold,
+//                       )),
+//                     ),
+//                   ),
+//                   SizedBox(height: 15),
+//                   CupertinoButton(
+//                     onPressed: () {
+//                       stopwatch.reset();
+//                     },
+//                     padding: EdgeInsets.all(0),
+//                     child: Text("Reset", style: TextStyle(
+//                       color: Colors.red,
+//                       fontWeight: FontWeight.bold,
+//                     )),
+//                   ),
+//                   SizedBox(height: 15),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(builder: (context) => ReportPage()),
+//                       );
+//                     },
+//                     child: Text("End Workout Session"),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
