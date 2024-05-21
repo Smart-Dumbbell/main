fa367fa4e2183ca101110fde8f8f3fc8ab398d57
ANYKBZ <alanz@V.attlocal.net>
2024-05-20 23:00:44 -0700

    create json file

--- Mobile/lib/global.dart
+++ Mobile/lib/global.dart
@@ -1,3 +1,5 @@
 library globals;
 
 int repetitions_count = 0;
+int bicep_repetitions_count = 0;
+int shoulder_repetitions_count = 0;
--- Mobile/lib/start_page.dart
+++ Mobile/lib/start_page.dart
@@ -1,144 +1,146 @@
-import 'package:flutter/material.dart';
-import 'package:smart_dumbbell_mobile/main.dart'; 
-import 'package:smart_dumbbell_mobile/working_page.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'package:logger/logger.dart' as myLogger;
-import 'package:smart_dumbbell_mobile/global.dart';
-//int repetitions = 0;
+//global variable code
 
-void updateRepetitions(int newReps) {
-  repetitions_count = newReps;
-}
+// import 'package:flutter/material.dart';
+// import 'package:smart_dumbbell_mobile/main.dart'; 
+// import 'package:smart_dumbbell_mobile/working_page.dart';
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+// import 'package:logger/logger.dart' as myLogger;
+// import 'package:smart_dumbbell_mobile/global.dart';
+// //int repetitions = 0;
 
-void resetRepetitions() {
-  repetitions_count = 0;
-}
+// void updateRepetitions(int newReps) {
+//   repetitions_count = newReps;
+// }
 
-class StartPage extends StatefulWidget {
-  @override
-  _StartPageState createState() => _StartPageState();
-}
+// void resetRepetitions() {
+//   repetitions_count = 0;
+// }
 
-class _StartPageState extends State<StartPage> {
-  final _ble = FlutterReactiveBle();
-  StreamSubscription<DiscoveredDevice>? _scanSub;
-  StreamSubscription<ConnectionStateUpdate>? _connectSub;
-  StreamSubscription<List<int>>? _notifySub;
-  var _found = false;
-  var _isLoading = false;
-  final logger = myLogger.Logger();
+// class StartPage extends StatefulWidget {
+//   @override
+//   _StartPageState createState() => _StartPageState();
+// }
 
-  @override
-  void dispose() {
-    _notifySub?.cancel();
-    _connectSub?.cancel();
-    _scanSub?.cancel();
-    super.dispose();
-  }
+// class _StartPageState extends State<StartPage> {
+//   final _ble = FlutterReactiveBle();
+//   StreamSubscription<DiscoveredDevice>? _scanSub;
+//   StreamSubscription<ConnectionStateUpdate>? _connectSub;
+//   StreamSubscription<List<int>>? _notifySub;
+//   var _found = false;
+//   var _isLoading = false;
+//   final logger = myLogger.Logger();
 
-  void _onScanUpdate(DiscoveredDevice d) {
-    if (d.name == 'BLE-TEMP' && !_found) {
-      logger.d('Scan done!');
-      _found = true;
-      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
-        if (update.connectionState == DeviceConnectionState.connected) {
-          setState(() {
-            isBluetoothConnected.value = true;
-            _isLoading = false;
-          });
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => WorkingPage()),
-          );
-          _onConnected(d.id);
-        } else if (update.connectionState == DeviceConnectionState.disconnected) {
-          setState(() {
-            isBluetoothConnected.value = false;
-            _isLoading = false;
-          });
-        }
-      });
-    }
-  }
+//   @override
+//   void dispose() {
+//     _notifySub?.cancel();
+//     _connectSub?.cancel();
+//     _scanSub?.cancel();
+//     super.dispose();
+//   }
 
-  void _onConnected(String deviceId) {
-    logger.d('On Connect!');
-    final characteristic = QualifiedCharacteristic(
-      deviceId: deviceId,
-      serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
-      characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
-    );
+//   void _onScanUpdate(DiscoveredDevice d) {
+//     if (d.name == 'BLE-TEMP' && !_found) {
+//       logger.d('Scan done!');
+//       _found = true;
+//       _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+//         if (update.connectionState == DeviceConnectionState.connected) {
+//           setState(() {
+//             isBluetoothConnected.value = true;
+//             _isLoading = false;
+//           });
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(builder: (context) => WorkingPage()),
+//           );
+//           _onConnected(d.id);
+//         } else if (update.connectionState == DeviceConnectionState.disconnected) {
+//           setState(() {
+//             isBluetoothConnected.value = false;
+//             _isLoading = false;
+//           });
+//         }
+//       });
+//     }
+//   }
 
-    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
-      setState(() {
-        value = const Utf8Decoder().convert(bytes);
-        logger.d(value);
-        _parseAndSaveRepetitions(value);
-      });
-    });
-  }
+//   void _onConnected(String deviceId) {
+//     logger.d('On Connect!');
+//     final characteristic = QualifiedCharacteristic(
+//       deviceId: deviceId,
+//       serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+//       characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
+//     );
 
-  void _parseAndSaveRepetitions(String data) {    // parse string and save int
-    final regex = RegExp(r'\d+');
-    final match = regex.firstMatch(data);
-    if (match != null) {
-      final repetitions_count = int.parse(match.group(0)!);
-      updateRepetitions(repetitions_count);
-    }
-  }
+//     _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+//       setState(() {
+//         value = const Utf8Decoder().convert(bytes);
+//         logger.d(value);
+//         _parseAndSaveRepetitions(value);
+//       });
+//     });
+//   }
 
-  void _startBluetoothScan() {
-    _scanSub?.cancel();
-    _found = false;
-    setState(() {
-      _isLoading = true;
-    });
-    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
-  }
+//   void _parseAndSaveRepetitions(String data) {    // parse string and save int
+//     final regex = RegExp(r'\d+');
+//     final match = regex.firstMatch(data);
+//     if (match != null) {
+//       final repetitions_count = int.parse(match.group(0)!);
+//       updateRepetitions(repetitions_count);
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Smart Dumbbell'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          // Start button
-          Align(
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                resetRepetitions(); // Reset repetitions when starting a new workout
-                _startBluetoothScan();
-              },
-              style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(120), // Adjust the size of the button
-              ),
-              child: Text('START', style: TextStyle(fontSize: 50)), // Adjust the size of the font
-            ),
-          ),
-          // Loading indicator
-          if (_isLoading)
-            Container(
-              color: Colors.white,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-}
+//   void _startBluetoothScan() {
+//     _scanSub?.cancel();
+//     _found = false;
+//     setState(() {
+//       _isLoading = true;
+//     });
+//     _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Smart Dumbbell'),
+//       ),
+//       body: Stack(
+//         children: <Widget>[
+//           // Start button
+//           Align(
+//             alignment: Alignment.center,
+//             child: ElevatedButton(
+//               onPressed: () {
+//                 resetRepetitions(); // Reset repetitions when starting a new workout
+//                 _startBluetoothScan();
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 shape: CircleBorder(),
+//                 padding: EdgeInsets.all(120), // Adjust the size of the button
+//               ),
+//               child: Text('START', style: TextStyle(fontSize: 50)), // Adjust the size of the font
+//             ),
+//           ),
+//           // Loading indicator
+//           if (_isLoading)
+//             Container(
+//               color: Colors.white,
+//               child: Center(
+//                 child: CircularProgressIndicator(),
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 
 
 
-
+//old code
 
 // import 'package:flutter/material.dart';
 // import 'package:smart_dumbbell_mobile/main.dart';
@@ -309,3 +311,167 @@ class _StartPageState extends State<StartPage> {
 //   }
 // }
 
+
+
+//json code
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:logger/logger.dart' as myLogger;
+import 'package:path_provider/path_provider.dart';
+import 'package:smart_dumbbell_mobile/global.dart';
+import 'package:smart_dumbbell_mobile/main.dart'; 
+import 'package:smart_dumbbell_mobile/working_page.dart';
+
+// int bicep_repetitions_count = 0;
+// int shoulder_repetitions_count = 0;
+
+Future<File> get _localFile async {
+  final directory = await getApplicationDocumentsDirectory();
+  return File('${directory.path}/repetitions.json');
+}
+
+Future<File> writeRepetitions() async {
+  final file = await _localFile;
+  return file.writeAsString(jsonEncode({
+    'bicep_repetitions': bicep_repetitions_count,
+    'shoulder_repetitions': shoulder_repetitions_count,
+  }));
+}
+
+void updateRepetitions(String type, int newReps) async {
+  if (type == 'Bicep') {
+    bicep_repetitions_count = newReps;
+  } else if (type == 'Shoulder') {
+    shoulder_repetitions_count = newReps;
+  }
+  await writeRepetitions();
+}
+
+void resetRepetitions() async {
+  bicep_repetitions_count = 0;
+  shoulder_repetitions_count = 0;
+  await writeRepetitions();
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final _ble = FlutterReactiveBle();
+  StreamSubscription<DiscoveredDevice>? _scanSub;
+  StreamSubscription<ConnectionStateUpdate>? _connectSub;
+  StreamSubscription<List<int>>? _notifySub;
+  var _found = false;
+  var _isLoading = false;
+  final logger = myLogger.Logger();
+
+  @override
+  void dispose() {
+    _notifySub?.cancel();
+    _connectSub?.cancel();
+    _scanSub?.cancel();
+    super.dispose();
+  }
+
+  void _onScanUpdate(DiscoveredDevice d) {
+    if (d.name == 'BLE-TEMP' && !_found) {
+      logger.d('Scan done!');
+      _found = true;
+      _connectSub = _ble.connectToDevice(id: d.id).listen((update) {
+        if (update.connectionState == DeviceConnectionState.connected) {
+          setState(() {
+            isBluetoothConnected.value = true;
+            _isLoading = false;
+          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => WorkingPage()),
+          );
+          _onConnected(d.id);
+        } else if (update.connectionState == DeviceConnectionState.disconnected) {
+          setState(() {
+            isBluetoothConnected.value = false;
+            _isLoading = false;
+          });
+        }
+      });
+    }
+  }
+
+  void _onConnected(String deviceId) {
+    logger.d('On Connect!');
+    final characteristic = QualifiedCharacteristic(
+      deviceId: deviceId,
+      serviceId: Uuid.parse('00000000-5EC4-4083-81CD-A10B8D5CF6EC'),
+      characteristicId: Uuid.parse('00000001-5EC4-4083-81CD-A10B8D5CF6EC'),
+    );
+
+    _notifySub = _ble.subscribeToCharacteristic(characteristic).listen((bytes) {
+      setState(() {
+        value = const Utf8Decoder().convert(bytes);
+        logger.d(value);
+        _parseAndSaveRepetitions(value);
+      });
+    });
+  }
+
+  void _parseAndSaveRepetitions(String data) {    // parse string and save int
+    final regex = RegExp(r'(Bicep|Shoulder) (\d+)');
+    final match = regex.firstMatch(data);
+    if (match != null) {
+      final type = match.group(1)!;
+      final repetitions_count = int.parse(match.group(2)!);
+      updateRepetitions(type, repetitions_count);
+    }
+  }
+
+  void _startBluetoothScan() {
+    _scanSub?.cancel();
+    _found = false;
+    setState(() {
+      _isLoading = true;
+    });
+    _scanSub = _ble.scanForDevices(withServices: []).listen(_onScanUpdate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart Dumbbell'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Start button
+          Align(
+            alignment: Alignment.center,
+            child: ElevatedButton(
+              onPressed: () {
+                resetRepetitions(); // Reset repetitions when starting a new workout
+                _startBluetoothScan();
+              },
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(120), // Adjust the size of the button
+              ),
+              child: Text('START', style: TextStyle(fontSize: 50)), // Adjust the size of the font
+            ),
+          ),
+          // Loading indicator
+          if (_isLoading)
+            Container(
+              color: Colors.white,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
